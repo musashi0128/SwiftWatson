e55542f4c693aecf306d21964a1636f33df2f09e
KazuhikoMiyamoto <k.miyamoto.0128@gmail.com>
2016-12-07 03:53:32 +0900

    Initial Commit

---
+++ SwiftWatson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWatson1
+//
+//  Created by 宮本一彦 on 2016/12/07.
+//  Copyright © 2016年 宮本一彦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
